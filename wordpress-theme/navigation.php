f237c243f0adc6437dae190473d80cdeb669b6f6
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-05-19 12:44:27 -0400

    Enabling smart links

--- wordpress-theme/navigation.php
+++ wordpress-theme/navigation.php
@@ -1,12 +1,14 @@
 			<div class="sixteen columns">
 				<ul class="next-nav">
 					<li class="next-us-flag">Official US Government Website</li>
-					<li class="next-primary"><a href="http://54.225.112.145/">Data.Gov</a></li>
-					<li><a href="#">Agencies</a></li>
-					<li><a href="#">Open Data 500</a></li>
-					<li><a href="#">Metrics</a></li>
-					<li><a href="#">Local</a></li>
-					<li><a href="#">Global</a></li>
-					<li><a href="#">About</a></li>
+					<li class="next-primary"><a href="/">Data.Gov</a></li>
+					
+					<!-- Pulling in Global Links from WP -->
+					<?php
+					$args = array(
+						'category_name'=>'primary', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+					wp_list_bookmarks($args); ?>
+
 				</ul><!-- nav -->
 			</div> <!-- sixteen columns -->
+
