9bc589e40407192beb6426a312adbd34f4564c97
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-06-10 18:04:24 -0400

    Autocomplete in template

--- wordpress-theme/primary-search.php
+++ wordpress-theme/primary-search.php
@@ -1,12 +1,12 @@
 			<div class="sixteen columns">
 				<div class="next-logo">
-					<div class="next-background"></div>
-					<div class="next-object">Data.Gov</div>
+					<div id="next-logo-title-bg" class="next-background"></div>
+					<div class="next-object"><span id="next-logo-title">Data.Gov</span></div>
 				</div>
 				<div class="next-stats">
 					<div class="next-background"></div>
 					<div id="rotate-stats" class="next-object" data-strings='
-    { "targets" : ["120,303 datasets","1,830 TB of data", "Search 400 APIs", "120,303 datasets"]}'>&nbsp;</div>
+    { "targets" : ["120,303 datasets","1,830 TB of data", "Search 400 APIs", "120,303 datasets", "CKAN Powered"]}'>&nbsp;</div>
 				</div>
 				<div class="next-search">
 					<div class="next-background"></div>
@@ -19,7 +19,7 @@
 						</div>
 						<div class="next-search-input">
 							<form method="get" action="http://54.225.111.163/dataset">
-	  							<input class="next" name="q" type="text" />
+	  							<input id="tags" class="next" name="q" type="text" />
 								<input type="submit" style="position: absolute; left: -9999px; width: 1px; height: 1px;"/>
 							</form>
 	  					</div>
@@ -29,14 +29,14 @@
 					<div class="next-background"></div>
 					<div class="next-object">
 						<ul>
-							<li><a href="#">Agriculture</a></li>
-							<li><a href="#">Commerce</a></li>
 							<li><a href="education">Education</a></li>
 							<li><a href="energy">Energy</a></li>
 							<li><a href="finance">Finance</a></li>
-							<li><a href="#">Global Development</a></li>
+							<li><a href="development">Global Development</a></li>
 							<li><a href="health">Health</a></li>
-							<li><a href="#">Safety</a></li>
+							<li><a href="health">Research</a></li>
+							<li><a href="safety">Safety</a></li>
+							<li><a href="#">all communities</a></li>
 						</ul>
 					</div>
 				</div>
