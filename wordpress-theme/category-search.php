e45970ff08249ba64eed1c3e47d2df6866e08dab
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-06-10 18:05:01 -0400

    Adding autocomplete to category pages

--- wordpress-theme/category-search.php
+++ wordpress-theme/category-search.php
@@ -1,7 +1,7 @@
 			<div class="sixteen columns">
 				<div class="next-logo">
-					<div class="next-background"></div>
-					<div class="next-object"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></div>
+					<div id="next-logo-title-bg" class="next-background"></div>
+					<div class="next-object"><span id="next-logo-title"><?php $category = get_the_category(); echo $category[0]->cat_name; ?></span></div>
 				</div>
 				<div class="next-search">
 					<div class="next-background"></div>
@@ -14,7 +14,7 @@
 						</div>
 						<div class="next-search-input">
 							<form method="get" action="http://54.225.111.163/dataset">
-	  							<input class="next" name="q" type="text" />
+	  							<input id="tags" class="next" name="q" type="text" />
 								<input type="submit" style="position: absolute; left: -9999px; width: 1px; height: 1px;"/>
 							</form>
 	  					</div>
