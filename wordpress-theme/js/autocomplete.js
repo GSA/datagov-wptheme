01d2008aba51f39e3e66eaa3b7ec805fbfdb55f3
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-06-10 18:03:54 -0400

    Adding auto-complete
    Still need strings

---
+++ wordpress-theme/js/autocomplete.js
@@ -0,0 +1,29 @@
+$(function() {
+var availableTags = [
+  "ActionScript",
+  "AppleScript",
+  "Asp",
+  "BASIC",
+  "C",
+  "C++",
+  "Clojure",
+  "COBOL",
+  "ColdFusion",
+  "Erlang",
+  "Fortran",
+  "Groovy",
+  "Haskell",
+  "Java",
+  "JavaScript",
+  "Lisp",
+  "Perl",
+  "PHP",
+  "Python",
+  "Ruby",
+  "Scala",
+  "Scheme"
+];
+$( "#tags" ).autocomplete({
+  source: availableTags
+});
+});
