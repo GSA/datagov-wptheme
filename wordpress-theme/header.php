76fb0f02a616f6ed6c57811c1addeb93f1414da7
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-07-01 11:03:52 -0700

    Adding Favicon
    Fixes #23

--- wordpress-theme/header.php
+++ wordpress-theme/header.php
@@ -41,9 +41,6 @@
 
 	<!-- Favicons
 	================================================== -->
-	<link rel="shortcut icon" href="images/favicon.ico">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	<link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/images/favicon.ico">
 
 </head>
