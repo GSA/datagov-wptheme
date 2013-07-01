d3b90249caa8db8c653c8dd917b0bd4eede7fd57
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-07-01 08:35:49 -0700

    Fixing footer links

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -1,6 +1,6 @@
 			<div class="sixteen columns footer">
 				<div class="twelve columns alpha">
-					<p><a href="#">Accessibility</a> <a href="#">Data Policy</a> <a href="#">Privacy Policy</a> <a href="https://github.com/GSA/datagov-design/">Improve this site</a></p>
+					<p><a href="http://www.data.gov/accessibility">Accessibility</a> <a href="http://www.data.gov/data-policy">Data Policy</a> <a href="http://www.data.gov/privacy-policy">Privacy Policy</a> <a href="https://github.com/GSA/datagov-design/">Improve this site</a></p>
 				</div>
 				<div class="four columns omega right-align">
 					<p>Next.Data.Gov</p>
