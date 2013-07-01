96aa671fa4f94fa8ff7a8dd0dd9e8fe87307137b
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2013-06-30 18:04:40 -0700

    Updating Viz
    Fixes #7 and #6

--- wordpress-theme/js/v1.js
+++ wordpress-theme/js/v1.js
@@ -12,7 +12,7 @@ var globalData;
 //	d3.csv("http://earthquake.usgs.gov/earthquakes/feed/v0.1/summary/1.0_week.csv")
 
 function loadData(){
-	d3.csv("earthquakes.csv")
+	d3.csv("http://54.225.112.145/wp-content/themes/datagov-jun13a/assets/earthquakes.csv")
 			.row(function(d){ return {latitude: +d.Latitude, longitude: +d.Longitude, depth: +d.Depth, magnitude: +d.Magnitude};})
 			.get(function(error, rows) {drawData(rows);});
 }
@@ -26,28 +26,6 @@ function drawData(loadedData) {
 		.attr("height", 450)
 		.attr("class", "chart");
 
-	/* chart.selectAll("circle")
-		.data(loadedData)
-		.enter().insert("svg:circle")
-			.attr("cx", function (d,i) {return longRange(d.longitude);})
-			.attr("cy", function (d,i) {return latRange(-1 * d.latitude);})
-			.attr("r", function (d,i) {return magRange(d.magnitude);})
-			.style("fill-opacity", 0.6)
-			.style("fill", function (d,i) { return magColor(d.magnitude)})
-			.on("mouseover", function (d,i) { 
-				d3.select(this)
-					.transition()
-					.delay(0)
-					.duration(300)
-					.attr("r", function (d,i) {return 5 * magRange(d.magnitude);});})
-			.on("mouseout", function (d,i){
-				d3.select(this)
-				.transition()
-				.delay(100)
-				.duration(200)
-				.attr("r", function (d,i) {return magRange(d.magnitude);} );
-			}); */
-
 		chart.selectAll("rect")
 		.data(loadedData)
 		.enter().append("rect")
@@ -58,6 +36,16 @@ function drawData(loadedData) {
 			.style("fill", "white")
 			.style("fill-opacity", function (d,i) {return o(loadedData[i].depth);})
 		;
+
+
+
+		chart.append("text")
+			.attr("x", 765)
+            .attr("y", 430)
+            .attr("font-size", "14px")
+            .attr("font-family", "'Lato', 'HelveticaNeue', 'Helvetica Neue', Helvetica, Arial, sans-serif")
+            .style("fill", "#424242")
+            .text("Visualization of Earthquake Data from USGS")
 
 		updateBars()
 }
@@ -75,69 +63,6 @@ function updateBars(){
 	console.log("Updating Bars");
 
 	setTimeout (updateBars, 800);
-}
-
-function scaleCircle(){
-	d3.select(this)
-		.transition()
-		.delay(0)
-		.duration(300)
-		.attr("r", 20);
-}
-
-function shrinkCircle(d,i){
-	d3.select(this)
-		.transition()
-		.delay(0)
-		.duration(300)
-		.attr("r", 10);
-}
-
-function update() {
-	var circles = viz.selectAll("circle")
-		.data(randomData(), function (d) { return d.id;});
-
-	circles.enter()
-		.insert("svg:circle")
-			.attr("cx", function (d) {return xRange(d.value1);})
-			.attr("cy", function (d) {return yRange(d.value2);})
-			.style("fill-opacity", 0.6)
-			.style("fill", "red");
-
-	circles.transition().duration(1000)
-		.ease("exp-in-out")
-		.attr("cx", function (d) {return xRange(d.value1);})
-		.attr("cy", function (d) {return yRange(d.value2);})
-		.attr("r",  function (d) {return d.value3;});
-
-	circles.exit ()
-		.transition().duration(1000)
-			.ease("exp-in-out")
-			.attr("r", 0)
-				.remove();
-
-	setTimeout (update, 2000);
-}
-
-function randomData () {
-	var n = randomRange (1, 20),
-		dataset = [];
-
-	for (var i = 0; i < n; i++) {
-		var data = {
-			id: i,
-			value1: randomRange (0, 400),
-			value2: randomRange (0, 400),
-			value3: randomRange (3, 20)
-		}
-		dataset.push (data);
-	}
-
-	return dataset;
-}
-
-function randomRange (min, max) {
-	return Math.round (Math.random() * (max - min) + min); 
 }
 
 loadData();
