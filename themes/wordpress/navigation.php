ba02e1676a99823a80a52456b0c5d274e6329b12
kapil bulchandani <kapil.bulchandani@reisys.com>
2013-11-20 18:11:49 -0500

    removing the login link logo from navigation

--- themes/wordpress/navigation.php
+++ themes/wordpress/navigation.php
@@ -8,10 +8,6 @@
         $args = array(
             'category_name'=>'primary', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
         wp_list_bookmarks($args); ?>
-        <span id="login" class="login">
-            <a href="/wp-admin" style="height:15px;">&nbsp;
-                <img src="<?php echo get_bloginfo('template_directory'); ?>/images/loginimage.png" height="20px" width="20px" alt="Login Link">
-            </a>
-        </span>
+
     </ul><!-- nav -->
 </div> <!-- sixteen columns -->
