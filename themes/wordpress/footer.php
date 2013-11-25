33c21aeae8e22cfce0221e76c59af0f47339e785
ullasjos <ujoseph@reisystems.com>
2013-11-25 09:49:13 -0500

    added dynamic login id and logout id for exclude param for the wp list function.

--- themes/wordpress/footer.php
+++ themes/wordpress/footer.php
@@ -2,10 +2,17 @@
     <div class="twelve columns alpha">
         <p>
             <?php
+            $page = get_bookmarks(array('category_name' => 'footer'));
+            foreach ($page as $bookmark){
+                if($bookmark->link_name == "Login")
+                    $loginid = $bookmark->link_id;
+                if($bookmark->link_name == "Logout")
+                    $logoutid = $bookmark->link_id;
+            }
             if ( is_user_logged_in() ) {
-                $args = array('category_name'=>'footer', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating','before'=>' ','after'=>' ','exclude'=>'336');
+                $args = array('category_name'=>'footer', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating','before'=>' ','after'=>' ','exclude'=>$loginid);
             } else {
-                $args = array('category_name'=>'footer', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating','before'=>' ','after'=>' ','exclude'=>'337');
+                $args = array('category_name'=>'footer', 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating','before'=>' ','after'=>' ','exclude'=>$logoutid);
             }
             wp_list_bookmarks($args);
             ?>
