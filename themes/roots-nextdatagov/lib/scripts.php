88c4db1896f03ed7102c9569588e6cee3d9b41f5
Philip Ashlock <phil@civicagency.org>
2013-11-25 02:26:41 -0500

    Updated css transitions for search input

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '78e25c3abf89aa4b9c86079862f02751');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a617f1b24785e514da6687e4eabc9ef3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
