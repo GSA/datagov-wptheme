5a66c556633f6e7ce048bd01473efb39d2ff9bcb
Philip Ashlock <phil@civicagency.org>
2013-11-25 03:09:42 -0500

    Updating CSS and base data for search input marquee

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -10,8 +10,9 @@
  * 2. /theme/assets/js/vendor/modernizr-2.7.0.min.js
  * 3. /theme/assets/js/main.min.js (in footer)
  */
-function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a617f1b24785e514da6687e4eabc9ef3');
+function roots_scripts() { 
+  wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d522377b16f177aad8e7fe1504f2679');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- themes/roots-nextdatagov/templates/searchform.php
+++ themes/roots-nextdatagov/templates/searchform.php
@@ -1,6 +1,7 @@
-<form role="search" method="get" class="search-form form-inline" action="<?php echo home_url('/'); ?>">
+<span id="search-prompt" class="col-md-5 col-lg-5">Search 89,234 datasets including</span>
+<form role="search" method="get" class="search-form form-inline col-md-7 col-lg-7 pull-right" action="<?php echo home_url('/'); ?>">
   <div class="input-group">
-    <input type="search" id="search-examples" data-strings='{ "targets" : ["91,101 datasets", "409 APIs", "349 apps","137 mobile apps","175 agencies"]}' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+    <input type="search" id="search-examples" data-strings='{ "targets" : ["Monthly House Price Indexes", "Health Care Provider Charge Data", "Credit Card Complaints", "Manufacturing & Trade Inventories & Sales","Federal Student Loan Program Data"]}' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
     <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"><?php _e('Search', 'roots'); ?></button>
