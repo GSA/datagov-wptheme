99d224b387af967d746bcee6032509300ceabfcf
shweta.shrivastava <shwetashri2880@gmail.com>
2013-11-26 11:07:03 -0500

    Search Text Box Dropdown issue fixed.

--- themes/wordpress/category-search.php
+++ themes/wordpress/category-search.php
@@ -27,7 +27,7 @@
                 <form method="get" action="http://catalog.data.gov/dataset">
                     <label for="Search_TextBox" class="hddn" title="Search Data.gov">Search Data.gov</label>
                     <input id="Search_TextBox" role="search" class="next" name="q" type="text" title="Start Searching"  onKeyUp="hidesearch();return false;"  >
-                    <a href="#" id="bottle" onMouseOver="displaysearch();return false;" ><span id="g-search-button"></span></a>
+                    <a href="#" id="bottle" onClick="displaysearch();return false;" ><span id="g-search-button"></span></a>
                     <div id="searchlist" style="display:none; ">
                         <label><input type="radio" id="SearchCatalog" name="SearchSite" value="" checked="true">&nbsp;&nbsp;Search Data Catalog</label><br>
                         <label><input type="radio" id="SearchSite" name="SearchSite" value="">&nbsp;&nbsp;Search Site Content</label><br>
@@ -46,24 +46,15 @@
     };
 </script>
 <script type="text/javascript">
-    $('#search-textbox').focus(function() {
+    $('#Search_TextBox').focusin(function() {
         $("#searchlist").show();
     });
-    $( document ).ready(function() {
-        $( '#search-textbox' ).focusout( function() {
-            $( '#searchlist' ).hover(
-                    function() {
-                        return;
-                    },
-                    function() {
-                        $( '#searchlist' ).fadeOut( 'slow' );
-
-                    });
-        });
+    $('#Search_TextBox').focusout(function() {
+        $( '#searchlist' ).fadeOut( 'slow' );
     });
-
-
-
+    $('#bottle').focusout(function() {
+        $( '#searchlist' ).fadeOut( 'slow' );
+    });
 </script>
 <script type="text/javascript">
     $('input[type="text"]').each(function(){
