ebcb61b3a7640298f7f76538b6e8ea5363d9ba76
Philip Ashlock <phil@civicagency.org>
2013-11-30 22:10:01 -0500

    First pass at implementing topic icons

--- themes/roots-nextdatagov/front-page.php
+++ themes/roots-nextdatagov/front-page.php
@@ -4,36 +4,39 @@
 This is the front-page.php template
 </p>
 
-<h1>Browse</h1>
+<h1>Browse Topics</h1>
 
-<?php $args = array(
-	'show_option_all'    => '',
-	'orderby'            => 'name',
-	'order'              => 'ASC',
-	'style'              => 'list',
-	'show_count'         => 0,
-	'hide_empty'         => 1,
-	'use_desc_for_title' => 1,
-	'child_of'           => 0,
-	'feed'               => '',
-	'feed_type'          => '',
-	'feed_image'         => '',
-	'exclude'            => '112,71,73,79,64,82,65,62,63,70,74,59,67,26880,102,93,69,61,57,60,72,94,56,26881,26879,81,68,75,26882,26883,26877',
-	'exclude_tree'       => '',
-	'include'            => '',
-	'hierarchical'       => 1,
-	'title_li'           => __( '' ),
-	'show_option_none'   => __('No categories'),
-	'number'             => null,
-	'echo'               => 1,
-	'depth'              => 0,
-	'current_category'   => 0,
-	'pad_counts'         => 0,
-	'taxonomy'           => 'category',
-	'walker'             => null
-); ?>
+<?php  
 
-<?php wp_list_categories( $args ); ?> 
+$args = array(
+	'type'                     => 'post',
+	'child_of'                 => 0,
+	'parent'                   => '',
+	'orderby'                  => 'name',
+	'order'                    => 'ASC',
+	'hide_empty'               => 1,
+	'hierarchical'             => 1,
+	'exclude'                  => '112,71,73,79,64,82,65,62,63,70,74,59,67,26880,102,93,69,61,57,60,72,94,56,26881,26879,81,68,75,26882,26883,26877',
+	'include'                  => '',
+	'number'                   => '',
+	'taxonomy'                 => 'category',
+	'pad_counts'               => false 
+
+);
+
+?>
+
+<ul class="topics">
+<?php 
+$categories = get_categories($args); 
+foreach ($categories as $category) {
+	$option = '<li class="topic-' . $category->category_nicename . '"><a href="/'.$category->category_nicename.'">';
+	$option .= $category->cat_name;
+	$option .= '</a></li>';
+	echo $option;
+}
+?>
+</ul> 
 
 <p>
 Show more
