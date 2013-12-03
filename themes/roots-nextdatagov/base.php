58af1a4c494066093e9dafa216ff60af8e463c4c
Victor Zapanta <victor.zapanta@gmail.com>
2013-12-03 13:30:46 -0500

    Removing container from base.php. Container should be declared at a lower level.

--- themes/roots-nextdatagov/base.php
+++ themes/roots-nextdatagov/base.php
@@ -20,7 +20,7 @@
     
   ?>
 
-  <div class="wrap container" role="document">
+  <div role="document">
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
