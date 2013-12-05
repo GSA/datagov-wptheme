1a9c7e05ccb1993d2a9d064e610ee3df15a9bc74
Philip Ashlock <phil@civicagency.org>
2013-12-05 17:06:13 -0500

    Separate parallel work on css

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,10 @@ function roots_scripts() {
   wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700');
   wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');  
   
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f178f6633ae7e14f2782ec0a754a5919');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a5913ddc0c32c92fcfa25de79c8d989e');
+
+  wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '');
+
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
