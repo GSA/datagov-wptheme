5b338d299fd9e572c1af6727029b53b21545accc
Victor Zapanta <victor.zapanta@gmail.com>
2013-12-06 00:31:35 -0500

    Killing lorem ipsum on highlights, killing sidebar.

--- themes/roots-nextdatagov/templates/content-highlights.php
+++ themes/roots-nextdatagov/templates/content-highlights.php
@@ -27,42 +27,15 @@ $highlight_posts = new WP_Query($args);
 
 <section id="highlights" class="wrap wrap-lightblue">
 <div class="container">
+    <div class="page-header">
+      <h1>Highlights</h1>
+    </div>
 
 <?php while ($highlight_posts->have_posts()) : $highlight_posts->the_post(); ?>
-    <div class="page-header">
-      <div class="col-md-12"><h1>Highlights</h1></div>
-    </div>
-    <div class="highlight">
-        <div class="col-md-12"><h2 class="entry-title"><?php the_title(); ?></h2></div>
-    	<div class="col-md-8">
+    <div class="highlight <?php get_category_by_slug( $slug ) ?>">
+        <h2 class="entry-title"><?php the_title(); ?></h2></div>
     		<?php the_content(); ?>                  
-    	</div>
-    	<div class="col-md-4">
-    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus</p>
-    		<button>Button One</button>
-    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus</p>
-    		<button>Button Two</button>
-    	</div>
     </div><!--/.highlight-->
-
-    <div class="highlight">
-        <div class="col-md-12"><h2 class="entry-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2></div>
-        <div class="col-md-12">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus. <a href="">Read more.</a>
-        </div>
-    </div><!--/.highlight-->
-
-    <div class="highlight">
-        <div class="col-md-12"><h2 class="entry-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2></div>
-        <div class="col-md-8">
-            <?php the_content(); ?>                  
-        </div>
-        <div class="col-md-4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra metus vitae iaculis mattis. Nam gravida dictum dui, sit amet congue odio pulvinar in. Quisque vitae dictum elit. In non leo quis tellus</p>
-            <button>Button One</button>
-        </div>
-    </div><!--/.highlight-->
-
 <?php endwhile; ?>
 
 <?php
