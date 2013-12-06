b9c9312592853157cc68a25e2bcfe7f5f379af61
Philip Ashlock <phil@civicagency.org>
2013-12-06 17:36:30 -0500

    Adding logo to footer

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,7 @@ function roots_scripts() {
   wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700');
   wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');  
   
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7ec966db5f7112ed686e99da5a3f0420');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6f1c8f1df640388a6f380390e0024c01');
 
   wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '');
 
