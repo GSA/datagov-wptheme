c41aee07def7ffaf4b765567e9d82dad40802e5c
kapil bulchandani <kapil.bulchandani@reisys.com>
2013-12-06 15:32:04 -0500

    adding wrap class to all the templates and adding nvaigation

--- themes/roots-nextdatagov/templates/content-search-results.php
+++ themes/roots-nextdatagov/templates/content-search-results.php
@@ -1,3 +1,20 @@
+<div class="container">
+<nav role="navigation" class="topic-subnav">
+               <ul class="nav navbar-nav"> 
+<?php
+	 // show Links associated to a community
+      // we need to build $args based either term_name or term_slug
+      $args = array(
+          'category_name'=> $term_slug, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+      wp_list_bookmarks($args);
+      if (strcasecmp($term_name,$term_slug)!=0) {
+          $args = array(
+              'category_name'=> $term_name, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+          wp_list_bookmarks($args);
+      }
+?>
+</ul></nav></div>
+
 <div class="single">
 <div class="container">
 
