7f3316411f1861c0d6c335b61573bb9a7d13510c
Philip Ashlock <phil@civicagency.org>
2013-12-06 14:37:54 -0500

    refactored header css

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,7 @@ function roots_scripts() {
   wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700');
   wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');  
   
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e71a23e76a42f3129922869af1d3b1e0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f3d5f4c75a46f674ad34e7f3517d308e');
 
   wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '');
 
