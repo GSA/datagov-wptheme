421965dac16025632e23013a5d7df671a733e160
kapil bulchandani <kapil.bulchandani@reisys.com>
2013-12-06 12:47:04 -0500

    changing logic of subnav

--- themes/roots-nextdatagov/category.php
+++ themes/roots-nextdatagov/category.php
@@ -44,7 +44,7 @@ if($subnav OR $subnav_extra):
         
         <?php endif; ?>
 
-        <?php if($subnav): ?>
+        <?php if(!$subnav): ?>
 
            <nav class="topic-subnav" role="navigation">
                <ul class="nav navbar-nav">         
