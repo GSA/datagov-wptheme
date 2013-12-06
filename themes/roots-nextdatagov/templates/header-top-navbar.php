91d4f1f598bf1432312d6c9fffb914411bbf884b
Victor Zapanta <victor.zapanta@gmail.com>
2013-12-05 23:45:29 -0500

    Left align jumbotron, remove column. Cleaning up spacing around getstarted.

--- themes/roots-nextdatagov/templates/header-top-navbar.php
+++ themes/roots-nextdatagov/templates/header-top-navbar.php
@@ -37,7 +37,7 @@
 
 <div class="jumbotron">
   <div class="container">
-    <div class="col-md-10 col-md-offset-1 text-center"><?php the_content(); ?></div>
+    <?php the_content(); ?>
   </div><!--/.container-->
 </div><!--/.jumbotron-->
 
@@ -46,8 +46,9 @@
 
 <div class="header banner frontpage-search">
     <div class="container">
-<div class="text-center getstarted"><h4>GET STARTED<br><small>SEARCH &gt; 90,000 DATASETS</small><br><i class="fa fa-caret-down"></i></h4></div>        <?php get_search_form(); ?>
-    </div>
+      <div class="text-center getstarted"><h4>GET STARTED<br><small>SEARCH &gt; 90,000 DATASETS</small><br /><i class="fa fa-caret-down"></i></h4></div>
+      <?php get_search_form(); ?>
+    </div><!--/.container-->
 </div>
 <?php endif; ?>
 
