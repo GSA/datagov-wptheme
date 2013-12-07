63071f41b9c27dedf16f207f452c51c119b3381d
kapil bulchandani <kapil.bulchandani@reisys.com>
2013-12-06 19:22:08 -0500

    adding the link menu to posts type

--- themes/roots-nextdatagov/templates/content-single.php
+++ themes/roots-nextdatagov/templates/content-single.php
@@ -1,3 +1,28 @@
+<?php
+$category = get_the_category();
+$term_name = $category[0]->cat_name;
+$term_slug = $category[0]->slug;
+?>
+<?php
+$cat_name = $category[0]->cat_name;
+$cat_slug = $category[0]->slug;
+?>
+<div class="container">
+    <nav role="navigation" class="topic-subnav">
+        <ul class="nav navbar-nav">
+            <?php
+            // show Links associated to a community
+            // we need to build $args based either term_name or term_slug
+            $args = array(
+                'category_name'=> $term_slug, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+            wp_list_bookmarks($args);
+            if (strcasecmp($term_name,$term_slug)!=0) {
+                $args = array(
+                    'category_name'=> $term_name, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+                wp_list_bookmarks($args);
+            }
+            ?>
+        </ul></nav></div>
 <div class="wrap container">
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
