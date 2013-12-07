472e76ad036e4107125fc4ef3ffa4deecbcc4c9d
ullasjos <ujoseph@reisystems.com>
2013-12-06 19:54:39 -0500

    removed the title

--- themes/roots-nextdatagov/templates/content-regional_planning.php
+++ themes/roots-nextdatagov/templates/content-regional_planning.php
@@ -43,7 +43,6 @@ $cat_slug = $category[0]->slug;
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
