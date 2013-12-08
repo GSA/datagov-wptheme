601ad736c585497c18e5eace1b8efc5e89243de5
Philip Ashlock <phil@civicagency.org>
2013-12-08 03:20:15 -0500

    include font awesome as less
    This allows mixins of font icon classes, but has some limitations with
    the way font awesome less is normally configured. See
    https://github.com/gruntjs/grunt-contrib-less/issues/113

--- themes/roots-nextdatagov/lib/scripts.php
+++ themes/roots-nextdatagov/lib/scripts.php
@@ -12,9 +12,9 @@
  */
 function roots_scripts() { 
   wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700');
-  wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');  
+  //wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');  
   
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cb91b655a783b148c06027ded5a60a52');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7ab63b000d3de1d01ab759749695d7b2');
 
   wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '');
 
