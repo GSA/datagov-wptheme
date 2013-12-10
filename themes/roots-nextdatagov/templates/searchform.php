b1a246877d14be3441d4ed705e0ecdc620b179a7
ullasjos <ujoseph@reisystems.com>
2013-12-10 11:31:04 -0500

    changed action to catalog site from usa search

--- themes/roots-nextdatagov/templates/searchform.php
+++ themes/roots-nextdatagov/templates/searchform.php
@@ -1,17 +1,9 @@
-<?php
-$category = get_the_category();
-if ($category[0]->cat_name != 'Uncategorized') {
-
-    $slug = $wp_query->query_vars['category_name'];
-}
-?>
-<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="/search-results/1/">
+<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="http://catalog.data.gov/dataset">
   <div class="input-group">
     <?php if(!is_front_page()): ?>
       <label for="search-header" class="hide"><?php _e('Search for:', 'roots'); ?></label>
     <?php endif; ?>    
     <input type="search" id="search-header" data-strings='{ "targets" : ["Monthly House Price Indexes", "Health Care Provider Charge Data", "Credit Card Complaints", "Manufacturing &amp; Trade Inventories &amp; Sales","Federal Student Loan Program Data"]}' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="q" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
-    <input type="hidden" name="group" value="<?php echo (get_category_by_slug($slug)->name) ? (get_category_by_slug($slug)->name) : 'site'; ?>">
       <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default">
            <i class="fa fa-search"></i>
