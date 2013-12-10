1610d456b0997acecaa64eaa91e3fc6585117b49
kapil bulchandani <kapil.bulchandani@reisys.com>
2013-12-09 22:36:26 -0500

    updating all the templates to add new navigation class

--- themes/roots-nextdatagov/templates/content-categories-blog.php
+++ themes/roots-nextdatagov/templates/content-categories-blog.php
@@ -7,22 +7,24 @@ $term_slug = $category[0]->slug;
 $cat_name = $category[0]->cat_name;
 $cat_slug = $category[0]->slug;
 ?>
-<div class="container">
-    <nav role="navigation" class="topic-subnav">
-        <ul class="nav navbar-nav">
-            <?php
-            // show Links associated to a community
-            // we need to build $args based either term_name or term_slug
-            $args = array(
-                'category_name'=> $term_slug, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
-            wp_list_bookmarks($args);
-            if (strcasecmp($term_name,$term_slug)!=0) {
+<div class="subnav banner">
+    <div class="container">
+        <nav role="navigation" class="topic-subnav">
+            <ul class="nav navbar-nav">
+                <?php
+                // show Links associated to a community
+                // we need to build $args based either term_name or term_slug
                 $args = array(
-                    'category_name'=> $term_name, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+                    'category_name'=> $term_slug, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
                 wp_list_bookmarks($args);
-            }
-            ?>
-        </ul></nav></div>
+                if (strcasecmp($term_name,$term_slug)!=0) {
+                    $args = array(
+                        'category_name'=> $term_name, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
+                    wp_list_bookmarks($args);
+                }
+                ?>
+            </ul></nav></div>
+</div>
 <div class="single">
 <div class="container">
 
