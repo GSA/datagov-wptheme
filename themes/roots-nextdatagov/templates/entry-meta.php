6dd17e7416254dc64d0b171d3dab8f9da52be8d3
Philip Ashlock <phil@civicagency.org>
2013-12-11 14:41:22 -0500

    only show name separator (bullet) if we have a name

--- themes/roots-nextdatagov/templates/entry-meta.php
+++ themes/roots-nextdatagov/templates/entry-meta.php
@@ -2,6 +2,8 @@
 	<p class="byline author vcard">
 	<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
 	</p>
+	<?php if (get_the_author()): ?>
 	 &bull;  
+	<?php endif; ?>
 	<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </div>
