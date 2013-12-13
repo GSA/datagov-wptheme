03b6754bbfd6b2eeeba99529a137729215db7edb
ullasjos <ujoseph@reisystems.com>
2013-12-12 20:57:58 -0500

    Dynamic Footers

--- plugins/datagov-custom/wp_download_links.php
+++ plugins/datagov-custom/wp_download_links.php
@@ -11,10 +11,17 @@ if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name 
     $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
     $menu_items = wp_get_nav_menu_items($menu->term_id);
     $json .= '"Footer":[';
+    $count = 0;
+    $count_menu_items = count($menu_items);
     foreach ( (array) $menu_items as $key => $menu_item ) {
-        $title = $menu_item->title;
-        $url = $menu_item->url;
-        $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($title).'",'.'"'.'link'.'":'.'"'.esc_attr($url).'"'.'}';
+        if($menu_item->menu_item_parent==0)
+            $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($menu_item->title).'",'.'"'.'link'.'":'.'"'.esc_attr($menu_item->url).'",'.'"'.'id'.'":'.'"'.$menu_item->db_id.'"'.'}';
+        else
+            $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($menu_item->title).'",'.'"'.'link'.'":'.'"'.esc_attr($menu_item->url).'",'.'"'.'id'.'":'.'"'.$menu_item->db_id.'",'.'"'.'parent_id'.'":'.'"'.$menu_item->menu_item_parent.'"'.'}';
+        if($count!=$count_menu_items-1)
+            $json .=",";
+
+        $count++;
     }
     $json .= ']';
 }
@@ -25,13 +32,75 @@ $menu_name = 'primary_navigation';
 if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
     $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
     $menu_items = wp_get_nav_menu_items($menu->term_id);
-    $json .= '"Primary":[';
+    $json .= ', "Primary":[';
+    $count = 0;
+    $count_menu_items = count($menu_items);
     foreach ( (array) $menu_items as $key => $menu_item ) {
-        $title = $menu_item->title;
-        $url = $menu_item->url;
-        $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($title).'",'.'"'.'link'.'":'.'"'.esc_attr($url).'"'.'}';
+        if($menu_item->menu_item_parent==0)
+            $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($menu_item->title).'",'.'"'.'link'.'":'.'"'.esc_attr($menu_item->url).'",'.'"'.'id'.'":'.'"'.$menu_item->db_id.'"'.'}';
+        else
+            $json .= '{'.'"'.'name'.'":'.'"'.esc_attr($menu_item->title).'",'.'"'.'link'.'":'.'"'.esc_attr($menu_item->url).'",'.'"'.'id'.'":'.'"'.$menu_item->db_id.'",'.'"'.'parent_id'.'":'.'"'.$menu_item->menu_item_parent.'"'.'}';
+        if($count!=$count_menu_items-1)
+            $json .=",";
+
+        $count++;
     }
     $json .= ']';
 }
-$json .= '};';
-print($json);
+$json .= '}';
+print(prettyPrint($json));
+
+
+function prettyPrint( $json ){
+    $result = '';
+    $level = 0;
+    $prev_char = '';
+    $in_quotes = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if( $char === '"' && $prev_char != '\\' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                $level--;
+                $ends_line_level = NULL;
+                $new_line_level = $level;
+                break;
+
+                case '{': case '[':
+                $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                $char = "";
+                $ends_line_level = $new_line_level;
+                $new_line_level = NULL;
+                break;
+            }
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "\n".str_repeat( "\t", $new_line_level );
+        }
+        $result .= $char.$post;
+        $prev_char = $char;
+    }
+
+    return $result;
+}
+
