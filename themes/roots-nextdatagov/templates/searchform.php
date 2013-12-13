3319845b5f3353407049f2758d7bfc07be25c990
Philip Ashlock <phil@civicagency.org>
2013-12-13 18:44:49 -0500

    Updated placeholder and title attributes for search input
    Addresses #213

--- themes/roots-nextdatagov/templates/searchform.php
+++ themes/roots-nextdatagov/templates/searchform.php
@@ -3,7 +3,17 @@
     <?php if(!is_front_page()): ?>
       <label for="search-header" class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
     <?php endif; ?>    
-    <input type="search" id="search-header" data-strings='{ "targets" : ["Monthly House Price Indexes", "Health Care Provider Charge Data", "Credit Card Complaints", "Manufacturing &amp; Trade Inventories &amp; Sales","Federal Student Loan Program Data"]}' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="q" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+
+    <?php 
+        $example_searches      = array("targets" => array("Monthly House Price Indexes", 
+                                                          "Health Care Provider Charge Data", 
+                                                          "Credit Card Complaints", 
+                                                          "Manufacturing &amp; Trade Inventories &amp; Sales",
+                                                          "Federal Student Loan Program Data"));     
+        $example_searches_text = 'Example searches: ' . implode(", ", $example_searches['targets']);
+    ?>
+
+    <input type="search" id="search-header" title="<?php echo $example_searches_text ?>" data-strings='<?php echo json_encode($example_searches); ?>' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="q" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
       <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default">
            <i class="fa fa-search"></i>
