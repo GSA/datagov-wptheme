a4e8a788121074d0c7f2e2ab0c80bcfa4afb1306
Philip Ashlock <phil@civicagency.org>
2013-12-13 16:44:57 -0500

    Make sure form label is visible to screen readers

--- themes/roots-nextdatagov/templates/searchform.php
+++ themes/roots-nextdatagov/templates/searchform.php
@@ -1,7 +1,7 @@
-<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="http://dev-ckan-fe-data.reisys.com/dataset">
+<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> no-padding col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="http://dev-ckan-fe-data.reisys.com/dataset">
   <div class="input-group">
     <?php if(!is_front_page()): ?>
-      <label for="search-header" class="hide"><?php _e('Search for:', 'roots'); ?></label>
+      <label for="search-header" class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
     <?php endif; ?>    
     <input type="search" id="search-header" data-strings='{ "targets" : ["Monthly House Price Indexes", "Health Care Provider Charge Data", "Credit Card Complaints", "Manufacturing &amp; Trade Inventories &amp; Sales","Federal Student Loan Program Data"]}' value="<?php if (is_search()) { echo get_search_query(); } ?>" name="q" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
       <span class="input-group-btn">
