bf21aa7a01ebe62f782fee5c557183f6a626be8f
kvuppala <kvuppala@reisystems.com>
2013-12-15 22:31:25 -0500

    fixing topic order and removing law per request

--- themes/roots-nextdatagov/front-page.php
+++ themes/roots-nextdatagov/front-page.php
@@ -42,9 +42,6 @@ $args = array(
 		<a href="/climate"><i></i><span>Climate</span></a>
 	</li>	
 	-->
-	<li class="topic-ocean">
-		<a href="/ocean"><i></i><span>Ocean</span></a>
-	</li>	
 	<li class="topic-education">
 		<a href="/education"><i></i><span>Education</span></a>
 	</li>
@@ -60,22 +57,24 @@ $args = array(
 	<li class="topic-development">
 		<a href="/development"><i></i><span>Global Development</span></a>
 	</li>
+	<li class="topic-health">
+		<a href="/health"><i></i><span>Health</span></a>
+	</li>
 	<li class="topic-jobs-skills">
 		<a href="/jobs-and-skills"><i></i><span>Jobs+Skills</span></a>
 	</li>
-	<li class="topic-health">
-		<a href="/health"><i></i><span>Health</span></a>
+	<li class="topic-ocean">
+		<a href="/ocean"><i></i><span>Ocean</span></a>
+	</li>	
+	<li class="topic-safety">
+		<a href="/safety"><i></i><span>Public Safety</span></a>
 	</li>
 	<li class="topic-research">
 		<a href="/research"><i></i><span>Science+Research</span></a>
 	</li>
-	<li class="topic-safety">
-		<a href="/safety"><i></i><span>Public Safety</span></a>
-	</li>
 	<li class="topic-weather">
 		<a href="/weather"><i></i><span>Weather</span></a>
 	</li>
-
 </ul> 
 
 
@@ -83,14 +82,23 @@ $args = array(
 
 
 <ul class="topics collapse" id="more-topics">
+	<li class="topic-business">
+		<a href="/business"><i></i><span>Business</span></a>
+	</li>
+	<li class="topic-cities">
+		<a href="/cities"><i></i><span>Cities</span></a>
+	</li>	
 	<li class="topic-consumer">
 		<a href="/consumer"><i></i><span>Consumer</span></a>
 	</li>	
+	<li class="topic-counties">
+		<a href="/counties"><i></i><span>Counties</span></a>
+	</li>
+	<li class="topic-developers">
+		<a href="/developers"><i></i><span>Developers</span></a>
+	</li>
 	<li class="topic-ethics">
 		<a href="https://explore.data.gov/ethics"><i></i><span>Ethics</span></a>
-	</li>
-	<li class="topic-law">
-		<a href="/law"><i></i><span>Law</span></a>
 	</li>
 	<li class="topic-manufacturing">
 		<a href="/manufacturing"><i></i><span>Manufacturing</span></a>
@@ -98,19 +106,6 @@ $args = array(
 	<li class="topic-states">
 		<a href="/states"><i></i><span>States</span></a>
 	</li>
-	<li class="topic-counties">
-		<a href="/counties"><i></i><span>Counties</span></a>
-	</li>
-	<li class="topic-cities">
-		<a href="/cities"><i></i><span>Cities</span></a>
-	</li>	
-	<li class="topic-developers">
-		<a href="/developers"><i></i><span>Developers</span></a>
-	</li>
-	<li class="topic-business">
-		<a href="/business"><i></i><span>Business</span></a>
-	</li>
-
 </ul>
 
 <div class="more-link">
