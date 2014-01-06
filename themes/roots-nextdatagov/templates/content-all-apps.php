ec4385f5993d9887465b36721807a13234138105
ullasjos <ujoseph@reisystems.com>
2014-01-06 17:45:21 -0500

    now displaying featured applications only

--- themes/roots-nextdatagov/templates/content-all-apps.php
+++ themes/roots-nextdatagov/templates/content-all-apps.php
@@ -64,205 +64,54 @@ while( have_posts() ) {
     <?php }?>
 </div>
 </div>
+
+<!-- Application featured taxonomy-->
 <?php
 $args = array(
     'post_type' => 'Applications',
     'tax_query'=>	array(
         'relation' => 'AND',
         array(
-            'taxonomy' => 'application_types',
-            'terms' => 'mobile-30',
+            'taxonomy' => 'featured',
             'field' => 'slug',
+            'terms' => array( 'highlights'),
         ),
 
     )
 );
-
-$apps = query_posts($args);
-
-if (!empty($apps)) { ?>
+$result = new WP_Query($args);
+if($result->found_posts> 0) { ?>
 <div class="Apps-wrapper">
-  <div class="Mobile-post" id="post-<?php the_ID(); ?>">
-    <div class="Appstitle" >Mobile Applications</div>
-    <div id="Appscontainer" class="appscontainer">
-      <ul class="bxslider">
+    <div class="Mobile-post" id="post-<?php $term->slug; ?>">
+        <div class="Appstitle" ><?php echo $term->name; ?> Applications</div>
         <?php
-    while( have_posts() ) {
-        the_post();
-
-
-
-        ?>
-        <li>
-            <div id="Appsitem" class="appsitem <?php the_ID();?>">
-                <div id="itemtitle"> <a href="<?php echo get_post_meta($post->ID, 'field_application_url', TRUE ); ?>">
-                    <?php the_title() ?>
-                </a> </div>
-                <div id="itemimage">
-                    <center>
-                        <?php
-                        $imagefile=get_field_object('field_5240b9c982f41');
-                        ?>
-                        <img class="scale-with-grid" src="<?php echo $imagefile['value']['url']; ?>" alt="<?php echo $imagefile['value']['alt']; ?>" style="height:85px;">
-                    </center>
-                </div>
-                <div id="itemcontent">
-                    <?php the_content() ?>
-                </div>
-                <?php //echo "Application URL:".get_post_meta($post->ID, 'field_application_url', TRUE ); ?>
-                <div id="bottomimages">
-                    <center>
-                        <?php if (strlen(get_post_meta($post->ID, 'field_ios_app_download_url', TRUE ))>10) :?>
-                        <a href="<?php echo get_post_meta($post->ID, 'field_ios_app_download_url', TRUE );?>">
-                            <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/ios.png" class="iconApp" alt="ios">
-                        </a>
-                        <?php endif; ?>
-                        <?php if (strlen(get_post_meta($post->ID, 'field_android_app_download_url', TRUE ))>10) :?>
-                        <a href="<?php echo get_post_meta($post->ID, 'field_android_app_download_url', TRUE );?>">
-                            <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/android.png" class="iconApp" alt="android">
-                        </a>
-                        <?php endif; ?>
-                        <?php if (strlen(get_post_meta($post->ID, 'field_windows_phone_app_download', TRUE ))>10) :?>
-                        <a href="<?php echo get_post_meta($post->ID, 'field_windows_phone_app_download', TRUE );?>">
-                            <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/windows.png" class="iconApp" alt="windows">
-                        </a>
-                        <?php endif; ?>
-                    </center>
-                </div>
-            </div>
-        </li>
-        <?php
-    }
-}
-?>
-</ul>
-</div>
-    <br clear="all" >
-</div>
-</div>
-<br clear="all" />
-
-<!-- Web Apps -->
-
-<?php
-$args = array(
-    'post_type' => 'Applications',
-    'tax_query'=>	array(
-        'relation' => 'AND',
-        array(
-            'taxonomy' => 'application_types',
-            'terms' => array('web-30','apps-30'),
-            'field' => 'slug',
-        ),
-
-    )
-);
-
-$apps = query_posts($args);
-if (!empty($apps)) { ?>
-<div class="Apps-wrapper">
-  <div class="Mobile-post" id="post-<?php the_ID(); ?>">
-    <div class="Appstitle" >Web Applications</div>
-    <?php
-    while( have_posts() ) {
-        the_post();
-        ?>
-        <div class="webcontainer <?php the_ID();?>">
-            <div id="webimage">
-                <?php
-                $imagefile=get_field_object('field_5240b9c982f41');
-                ?>
-                <img <?php if ($imagefile['value']['url'] ==''){ ?> class="scale-with-grid noImage"<?php }else{  ?> class="scale-with-grid" <?php }?> src="<?php echo $imagefile['value']['url']; ?>" alt="<?php echo $imagefile['value']['alt']; ?>"> </div>
-            <div id="webcontent">
-                <h2> <a href="<?php echo get_post_meta($post->ID, 'field_application_url', TRUE ); ?>">
-                    <?php the_title() ?>
-                </a> </h2>
-                <div class='content'>
-                    <div id="webtext">
-                        <?php the_content() ?>
+        while( $result->have_posts() ) {
+            $result->the_post();
+            ?>
+            <div class="webcontainer <?php the_ID();?>">
+                <div id="webimage">
+                    <?php
+                    $imagefile=get_field_object('field_5240b9c982f41');
+                    ?>
+                    <img class="scale-with-grid" src="<?php echo $imagefile['value']['url']; ?>" alt="<?php echo $imagefile['value']['alt']; ?>"> </div>
+                <div id="webcontent">
+                    <h2> <a href="<?php echo get_post_meta($post->ID, 'field_application_url', TRUE ); ?>">
+                        <?php the_title() ?>
+                    </a> </h2>
+                    <div class='content'>
+                        <div id="webtext">
+                            <?php the_content() ?>
+                        </div>
                     </div>
                 </div>
+                <br clear="all" />
+                <?php //echo "Application URL:".get_post_meta($post->ID, 'field_application_url', TRUE ); ?>
             </div>
-            <br clear="all" />
-            <?php //echo "Application URL:".get_post_meta($post->ID, 'field_application_url', TRUE ); ?>
-            <div id="bottomimages">
-                <center>
-                    <?php if (strlen(get_post_meta($post->ID, 'field_ios_app_download_url', TRUE ))>10) :?>
-                    <a href="<?php echo get_post_meta($post->ID, 'field_ios_app_download_url', TRUE );?>"> <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/ios.png" class="iconApp" alt="ios"> </a>
-                    <?php endif; ?>
-                    <?php if (strlen(get_post_meta($post->ID, 'field_android_app_download_url', TRUE ))>10) :?>
-                    <a href="<?php echo get_post_meta($post->ID, 'field_android_app_download_url', TRUE );?>"> <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/android.png" class="iconApp" alt="android" > </a>
-                    <?php endif; ?>
-                    <?php if (strlen(get_post_meta($post->ID, 'field_windows_phone_app_download', TRUE ))>10) :?>
-                    <a href="<?php echo get_post_meta($post->ID, 'field_windows_phone_app_download', TRUE );?>"> <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/img/windows.png" class="iconApp" alt="windows"> </a>
-                    <?php endif; ?>
-                </center>
-            </div>
-        </div>
-        <?php
-    }
-}
-?>
-    <br clear="all" />
-</div>
+            <?php
+        } ?>
+    </div>
 </div>
 <br clear="all" />
-<!-- Application categories taxonomy-->
-
-<?php
-$terms = get_terms("application_categories");
-$count = count($terms);
-if ( $count > 0 ){
-    foreach ( $terms as $term ) {
-        if($term->count >0){
-            $args = array(
-                'post_type' => 'Applications',
-                'tax_query'=>	array(
-                    'relation' => 'AND',
-                    array(
-                        'taxonomy' => 'application_categories',
-                        'terms' => $term->slug,
-                        'field' => 'slug',
-                    ),
-                )
-            );
-            $result = new WP_Query($args);
-            ?>
-            <div class="Apps-wrapper">
-              <div class="Mobile-post" id="post-<?php $term->slug; ?>">
-                <div class="Appstitle" ><?php echo $term->name; ?> Applications</div>
-                    <?php
-                    while( $result->have_posts() ) {
-                        $result->the_post();
-                        ?>
-                        <div class="webcontainer <?php the_ID();?>">
-                            <div id="webimage">
-                                <?php
-                                $imagefile=get_field_object('field_5240b9c982f41');
-                                ?>
-                                <img <?php if ($imagefile['value']['url'] ==''){ ?> class="scale-with-grid noImage"<?php }else{  ?> class="scale-with-grid" <?php }?> src="<?php echo $imagefile['value']['url']; ?>" alt="<?php echo $imagefile['value']['alt']; ?>"> </div>
-                            <div id="webcontent">
-                                <h2> <a href="<?php echo get_post_meta($post->ID, 'field_application_url', TRUE ); ?>">
-                                    <?php the_title() ?>
-                                </a> </h2>
-                                <div class='content'>
-                                    <div id="webtext">
-                                        <?php the_content() ?>
-                                    </div>
-                                </div>
-                            </div>
-                            <br clear="all" />
-                            <?php //echo "Application URL:".get_post_meta($post->ID, 'field_application_url', TRUE ); ?>
-                        </div>
-                        <?php
-                    }
-        }
-        ?>
-        <br clear="all" />
-              </div>
-            </div>
-                <br clear="all" />
-        <?php    }
-}
-?>
 </div>
+<?php }
+?>
