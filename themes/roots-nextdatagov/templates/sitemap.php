57046b436b9da2c3500f1eabea4a893df611c457
alex-perfilov-reisys <alexandr.perfilov@reisystems.com>
2014-01-06 16:21:45 -0500

    Sitemap changes

--- themes/roots-nextdatagov/templates/sitemap.php
+++ themes/roots-nextdatagov/templates/sitemap.php
@@ -6,13 +6,13 @@ Template Name: Sitemap
 <div class="container">
     <?php if (has_nav_menu('primary_navigation')) : ?>
         <h3>Header</h3>
-        <? wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'depth' => 1));
+        <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'depth' => 1));
     endif;
     ?>
 
     <?php if (has_nav_menu('topics_navigation')) : ?>
         <h3>Topics</h3>
-        <? wp_nav_menu(array('theme_location' => 'topics_navigation', 'menu_class' => 'nav'));
+        <?php wp_nav_menu(array('theme_location' => 'topics_navigation', 'menu_class' => 'nav'));
     endif;
     ?>
 
