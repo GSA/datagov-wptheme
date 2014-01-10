0cf846a15fc8676143fa0514c475ec501016a211
ullasjos <ujoseph@reisystems.com>
2014-01-09 23:05:26 -0500

    impletemented the pagination using the array method.

--- themes/roots-nextdatagov/templates/content-all-apps-pagination.php
+++ themes/roots-nextdatagov/templates/content-all-apps-pagination.php
@@ -53,73 +53,173 @@ $cat_slug = $category[0]->slug;
             </ul></nav></div>
 </div>
 <div class="container">
-<?php
-while( have_posts() ) {
-    the_post();
-    ?>
+    <?php
+    while( have_posts() ) {
+        the_post();
+        ?>
 <div class="Apps-wrapper">
   <div class="Apps-post" id="post-<?php the_ID(); ?>">
       <div  class="Appstitle" ><?php the_title();?></div>
-    <?php the_content();   ?>
-    <?php }?>
+        <?php the_content();   ?>
+        <?php }?>
 </div>
 </div>
 
-<!-- Application featured taxonomy-->
-<?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$args = array(
-    'orderby'=> 'modified',
-    'post_type' => 'Applications',
-    'post_status' => 'publish',
-    'posts_per_page' => 5,
-    'paged' => $paged
-);
-$result = new WP_Query($args);
-if($result->found_posts> 0) {  ?>
+    <!-- Application featured taxonomy-->
+    <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+    $args_featured = array(
+        'post_type' => 'Applications',
+        'posts_per_page' => -1,
+        'post_status'=>'publish',
+        'tax_query'=>	array(
+            'relation' => 'AND',
+            array(
+                'taxonomy' => 'featured',
+                'field' => 'slug',
+                'terms' => array( 'highlights'),
+            ),
+        ),
+    );
+
+    $args_nonfeatured = array(
+        'post_type' => 'Applications',
+        'posts_per_page' => -1,
+        'post_status'=>'publish',
+        'tax_query'=>	array(
+            'relation' => 'AND',
+            array(
+                'taxonomy' => 'featured',
+                'field' => 'slug',
+                'terms' => array( 'highlights'),
+                'operator' => 'NOT IN'
+            )
+        ),
+    );
+
+
+    $result_featured = new WP_Query($args_featured);
+    wp_reset_query();
+    $featured = array();
+    $i=0;
+    while( $result_featured->have_posts() ) {
+        $result_featured->the_post();
+        $featured[$i]['title']= get_the_title($post->ID);
+        $featured[$i]['conent']= get_the_content($post->ID);
+        $featured[$i]['field_application_url'] = get_post_meta($post->ID, 'field_application_url', TRUE );
+        $featured[$i]['image_url']=get_field_object('field_5240b9c982f41')['value']['url'];
+        $featured[$i]['image_alt']=get_field_object('field_5240b9c982f41')['value']['alt'];
+        $featured[$i]['featured']=true;
+        $i++;
+    }
+
+    $result_nonfeatured = new WP_Query($args_nonfeatured);
+    $not_featured = array();
+    $i=0;
+    while( $result_nonfeatured->have_posts() ) {
+        $result_nonfeatured->the_post();
+        $not_featured[$i]['title']= get_the_title($post->ID);
+        $not_featured[$i]['conent']= get_the_content($post->ID);
+        $not_featured[$i]['field_application_url'] = get_post_meta($post->ID, 'field_application_url', TRUE );
+        $not_featured[$i]['image_url']=get_field_object('field_5240b9c982f41')['value']['url'];
+        $not_featured[$i]['image_alt']=get_field_object('field_5240b9c982f41')['value']['alt'];
+        $not_featured[$i]['featured']=false;
+        $i++;
+    }
+    wp_reset_query();
+    $apparray=array_merge($featured,$not_featured);
+    $total_apps = count($apparray);
+    $apps_per_page = 10;
+    if(isset($apparray)) {
+        $total_pages = ceil($total_apps / $apps_per_page);
+    } else {
+        $total_pages = 1;
+        $total_apps = 0;
+    }
+    if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+        $currentpage = (int) $_GET['currentpage'];
+    } else {
+        $currentpage = 1;
+    }
+    if ($currentpage > $total_pages) {
+        $currentpage  = $total_pages;
+    }
+    if ($currentpage < 1) {
+        $currentpage = 1;
+    }
+    $start = ($currentpage - 1) * $apps_per_page + 1;
+    ?>
     <div class="Apps-wrapper">
         <div class="Mobile-post" id="post-<?php $term->slug; ?>">
             <div class="Appstitle" ></div>
             <?php
-            while( $result->have_posts() ) {
-                $result->the_post();
-                ?>
-                <div class="webcontainer <?php the_ID();?>">
-                    <div id="webimage">
-                        <?php
-                        $imagefile=get_field_object('field_5240b9c982f41');
-                        ?>
-                        <img class="scale-with-grid" src="<?php echo $imagefile['value']['url']; ?>" alt="<?php echo $imagefile['value']['alt']; ?>"> </div>
-                    <div id="webcontent">
-                        <h2> <a href="<?php
-                            echo get_post_meta($post->ID, 'field_application_url', TRUE ); ?>">
-                            <?php the_title()?>
-                        </a> </h2>
+            for($i=$start-1; $i<$start-1+$apps_per_page; $i++) {
+                if(isset($apparray[$i])) { ?>
+                    <div class="webcontainer <?php the_ID();?>">
+                        <div id="webimage">
+                            <img class="scale-with-grid" src="<?php echo $apparray[$i]['image_url']; ?>" alt="<?php echo $apparray[$i]['image_alt']; ?>">
+                        </div>
+                        <div id="webcontent">
+                            <h2> <a href="<?php
+                                echo $apparray[$i]['field_application_url']; ?>">
+                                <?php echo $apparray[$i]['title'];?>
+                            </a> </h2>
 
-                        <div class='content'>
-                            <div id="webtext">
-                                <?php the_content() ?>
+                            <div class='content'>
+                                <div id="webtext">
+                                    <?php echo $apparray[$i]['conent']; ?>
+                                </div>
                             </div>
                         </div>
+                        <br clear="all" />
                     </div>
-                    <br clear="all" />
-                    <?php //echo "Application URL:".get_post_meta($post->ID, 'field_application_url', TRUE ); ?>
-                </div>
-                <?php
-                $terms=wp_get_post_terms($post->ID, 'featured');
-                if(!empty($terms)) { ?>
-                    <div class="webfeatured"><p style="margin-top:1px;"><img width="30px" height="30px" src="/wp-content/themes/roots-nextdatagov/assets/img/featured.png">  Featured!</p></div>
                     <?php
+                    $terms=$apparray[$i]['featured'];
+                    if($terms) { ?>
+                        <div><p style="margin-top:1px;"><img width="30px" height="30px" src="/wp-content/themes/roots-nextdatagov/assets/img/featured.png">  Featured!</p></div>
+                        <?php
+                    }
                 }
-                ?>
-                <?php
-            } ?>
+            }
+            ?>
+            <br clear="all" />
         </div>
+
     </div>
-    <nav class="post-nav">
-        <?php your_pagination($result) ;?>
-    </nav>
-    <br clear="all" />
-</div>
-<?php }
-?>
+    <div class='pagination'>
+        <p class="counter">
+            <?php printf( __( 'Page %1$s of %2$s' ), $currentpage, $total_pages ); ?>
+        </p>
+        <ul class='pagination'>
+            <?php
+            $output = "";
+            if($total_apps > $apps_per_page) {
+                $range = 10;
+                if ($currentpage > 1) {
+                    //$output .= "<br clear='both'/><li class='pager-first first'><a class='prev page-numbers pagenav local-link' href='?currentpage=1'> FIRST </a></li> ";
+                    $prevpage = $currentpage - 1;
+                    $output .= "<li class='pagination-prev'><a class='prev page-numbers pagenav local-link' href='?currentpage=$prevpage'>Previous</a> </li>";
+                }
+                for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
+                    if (($x > 0) && ($x <= $total_pages)) {
+                        if ($x == $currentpage) {
+                            if ($total_pages > 1) {
+                                $output .= "<li><span class='page-numbers pagenav current'> $x </span></li>";
+                            }
+                        }
+                        else {
+                            $output .= "<li><a class='page-numbers pagenav' href='?currentpage=$x'> $x </a></li>";
+                        }
+                    }
+                }
+                if ($currentpage != $total_pages) {
+                    $nextpage = $currentpage + 1;
+                    $output .= " <li class='pagination-next'> <a href='?currentpage=$nextpage'> Next</a></li> ";
+                    //$output .= " <li class='pagination-next'><a href='?currentpage=$total_pages'>  LAST </a> </li>";
+                }
+            }
+            print $output;
+            ?>
+        </ul>
+    </div>
