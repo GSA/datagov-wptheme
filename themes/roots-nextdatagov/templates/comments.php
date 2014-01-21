0150fe2b060ffa0705fa058e85e22e4c7353e46d
kapil-bulchandani <kapil.bulchandani@reisystems.com>
2014-01-21 11:03:03 -0500

    commenting out the website field in comments

--- themes/roots-nextdatagov/templates/comments.php
+++ themes/roots-nextdatagov/templates/comments.php
@@ -62,10 +62,11 @@
             <label for="email"><?php _e('Email (will not be published)', 'roots'); if ($req) _e(' (required)', 'roots'); ?></label>
             <input type="email" class="form-control" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
           </div>
-          <div class="form-group">
-            <label for="url"><?php _e('Website', 'roots'); ?></label>
-            <input type="url" class="form-control" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22">
-          </div>
+          /* commenting out url field no required on front end */
+          <!--<div class="form-group">
+            <label for="url"><?php // _e('Website', 'roots'); ?></label>
+            <input type="url" class="form-control" name="url" id="url" value="<?php //echo esc_attr($comment_author_url); ?>" size="22">
+          </div>-->
         <?php endif; ?>
         <div class="form-group">
           <label for="comment"><?php _e('Comment', 'roots'); ?></label>
