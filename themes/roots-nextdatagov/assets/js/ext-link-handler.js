8125bdd3a22a31c48f09760a1ce3884fd43e4042
kapil-bulchandani <kapil.bulchandani@reisystems.com>
2014-01-21 12:11:14 -0500

    adding jquery tabs for ocean page

--- themes/roots-nextdatagov/assets/js/ext-link-handler.js
+++ themes/roots-nextdatagov/assets/js/ext-link-handler.js
@@ -46,4 +46,41 @@ jQuery( document ).ready(function() {
 jQuery(document).ready(function () {
 jQuery(".colorbox-inline").colorbox({inline:true, width:"50%"});
 });
+/* jQuery tabs for ocean page */
+jQuery(document).ready(function () {
 
+$('ul#content-nav').each(function(){
+  // For each set of tabs, we want to keep track of
+  // which tab is active and it's associated content
+  var $active, $content, $links = $(this).find('a');
+
+  // If the location.hash matches one of the links, use that as the active tab.
+  // If no match is found, use the first link as the initial active tab.
+  $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+  $active.addClass('active');
+  $content = $($active.attr('href'));
+
+  // Hide the remaining content
+  $links.not($active).each(function () {
+    $($(this).attr('href')).hide();
+  });
+
+  // Bind the click event handler
+  $(this).on('click', 'a', function(e){
+    // Make the old tab inactive.
+    $active.removeClass('active');
+    $content.hide();
+
+    // Update the variables with the new link and content
+    $active = $(this);
+    $content = $($(this).attr('href'));
+
+    // Make the tab active.
+    $active.addClass('active');
+    $content.show();
+
+    // Prevent the anchor's default click action
+    e.preventDefault();
+  });
+});
+});
