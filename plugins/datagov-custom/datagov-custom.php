9197588f413764621c941b8a303192d0d8c28024
ullasjos <ujoseph@reisystems.com>
2014-01-31 14:32:06 -0500

    added additional logic to show the links in the_excert function

--- plugins/datagov-custom/datagov-custom.php
+++ plugins/datagov-custom/datagov-custom.php
@@ -359,3 +359,26 @@ function datagov_custom_js() {
   }
 }
 add_action( 'admin_init','datagov_custom_js');
+
+function datagov_custom_keep_my_links($text) {
+    $raw_excerpt = $text;
+    if ( '' == $text ) {
+        $text = get_the_content('');
+        $text = strip_shortcodes( $text );
+        $text = apply_filters('the_content', $text);
+        $text = str_replace(']]>', ']]>', $text);
+        $text = strip_tags($text, '<a>');
+        $excerpt_length = apply_filters('excerpt_length', 55);
+        $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
+        $words = preg_split('/(<a.*?a>)|\n|\r|\t|\s/', $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE );
+        if ( count($words) > $excerpt_length ) {
+            array_pop($words);
+            $text = implode(' ', $words);
+            $text = $text . $excerpt_more;
+        }
+        else {
+            $text = implode(' ', $words);
+        }
+    }
+    return apply_filters('new_wp_trim_excerpt', $text, $raw_excerpt);
+}
