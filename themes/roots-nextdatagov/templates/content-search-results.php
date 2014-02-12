376073bc28e89658f7fa44bc5c25407ef140111c
ullasjos <ujoseph@reisystems.com>
2014-02-12 14:34:10 -0500

    appended sort params to the query

--- themes/roots-nextdatagov/templates/content-search-results.php
+++ themes/roots-nextdatagov/templates/content-search-results.php
@@ -44,7 +44,7 @@ if(isset($query) && isset($group) && $group == 'site')
 
 
 function usasearch_display_results($query = '', $group = ''){
-    echo "You are searching <strong>$query</strong> in entire site, show results in <a href='//catalog.data.gov/dataset?q=".stripslashes($query)."'> catalog.data.gov </a>. <br /><br />";
+    echo "You are searching <strong>$query</strong> in entire site, show results in <a href='//catalog.data.gov/dataset?q=".stripslashes($query)."&sort=score+desc%2C+name+asc'> catalog.data.gov </a>. <br /><br />";
     // current page number
     $parts = explode('/', $_SERVER['REQUEST_URI']);
     $cur_page = $parts[2];
