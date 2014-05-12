e1b5ea8eec3e53f7bffa6762aa990ec48a048b9e
Philip Ashlock <phil@civicagency.org>
2014-05-12 16:08:57 -0400

    CSS refinements for subnav dropdown menu

--- roots-nextdatagov/lib/scripts.php
+++ roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,7 @@ function roots_scripts() {
 	wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700' );
 	//wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
 
-	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '95c84deb9e1a27a28af5e0f29833eb14');
+	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '053c1c0af4d9aec2a1cb79dc5024c3dc');
 	wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '' );
 
 
