e70fee7aa762adb125095524d58fc9dd1c1eb656
Philip Ashlock <phil@civicagency.org>
2014-05-12 12:34:57 -0400

    Ensure topic intros work with sub categories

--- roots-nextdatagov/templates/category-intro.php
+++ roots-nextdatagov/templates/category-intro.php
@@ -3,10 +3,12 @@
 
 // See if there is a frontpage for the category.
 
+	$current_category_id = get_query_var( 'cat' );
+
 	$args = array(
 		'post_type'           => 'page',
 		'ignore_sticky_posts' => 1,
-		'cat'                 => get_query_var( 'cat' ),
+		'cat'                 => $current_category_id,
 		'tax_query'           => array(
 			array(
 				'taxonomy' => 'featured',
@@ -15,20 +17,32 @@
 				'operator' => 'IN'
 			)
 		),
-		'posts_per_page'      => 1
+		'posts_per_page'      => 10
 	);
 
 	$category_intro = new WP_Query( $args );
 
+	
+
 ?>
 
+<?php 
+while ( $category_intro->have_posts() ) : 
 
+	$category_intro->the_post(); 
+	$this_category = get_the_category(); 
 
-<?php while ( $category_intro->have_posts() ) : $category_intro->the_post(); ?>
-	<div class="intro">
-		<div class="container">
-			<?php the_content(); ?>
+	if($this_category[0]->term_id == $current_category_id):		
+?>
+
+		<div class="intro">
+			<div class="container">
+
+				<?php the_content(); ?>
+
+			</div>
 		</div>
-	</div>
+
+	<?php endif; ?>
 <?php endwhile; ?>
 
