d548949054a2b921e93c21af402c8dd93f60a85a
Philip Ashlock <phil@civicagency.org>
2014-05-14 13:38:58 -0400

    Use protocol free URI rather than detect current

--- roots-nextdatagov/templates/searchform.php
+++ roots-nextdatagov/templates/searchform.php
@@ -1,9 +1,10 @@
 <?php
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $ckan_default_server = (get_option('ckan_default_server') != '') ? get_option('ckan_default_server') : 'catalog.data.gov/dataset';
+$ckan_default_server = '//' . $ckan_default_server;
+
 
 ?>
-<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> no-padding col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="<?php echo $protocol.$ckan_default_server ?>">
+<form role="search" method="get" class="search-form form-inline<?php if(is_front_page()): ?> no-padding col-md-12 col-lg-12<?php else:?> navbar-right navbar-nav  col-sm-6 col-md-6 col-lg-6<?php endif;?>" action="<?php echo $ckan_default_server ?>">
   <div class="input-group">
     <?php if(!is_front_page()): ?>
       <label for="search-header" class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
