47c4a796031773cdb4b8e7511fcf646dcebd9a32
ullasjos <ujoseph@reisystems.com>
2014-05-15 16:02:05 -0400

    added author name in the summary: Include author for WordPress posts - 197

--- roots-nextdatagov/templates/content.php
+++ roots-nextdatagov/templates/content.php
@@ -4,14 +4,15 @@
         <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-summary">
-
         <?php
         remove_filter('get_the_excerpt', 'wp_trim_excerpt');
         add_filter('get_the_excerpt', 'datagov_custom_keep_my_links');
-
         $more_tag = strpos($post->post_content, '<!--more-->');
         ($more_tag) ? the_content('Continued') : the_excerpt();
+        $author_name = get_field('author_name');
+        if(empty($author_name))
+            $author_name = get_the_author();
         ?>
-
+        <em>By <?php echo $author_name;?></em>
     </div>
 </article>
