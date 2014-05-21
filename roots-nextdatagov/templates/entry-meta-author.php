afa5c766888dd32a1ef09ff0dab0b5c859b4043c
ullasjos <ujoseph@reisystems.com>
2014-05-21 17:57:07 -0400

    added the "By" along with vairable itself

--- roots-nextdatagov/templates/entry-meta-author.php
+++ roots-nextdatagov/templates/entry-meta-author.php
@@ -1,9 +1,9 @@
 <?php
 $additional_author_name = get_field('author_name');
 $author_name = get_the_author();
-$display_author_name = (!empty($additional_author_name)) ? $additional_author_name : $author_name;
+$display_author_name = (!empty($additional_author_name)) ? "By ".$additional_author_name : "By ".$author_name;
 ?>
 <div class="entry-meta" xmlns="http://www.w3.org/1999/html">
-    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?>&nbsp;&nbsp;<i>By <?php echo $display_author_name;?></i></time>
+    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?>&nbsp;&nbsp;<i><?php echo $display_author_name;?></i></time>
 </div>
 
