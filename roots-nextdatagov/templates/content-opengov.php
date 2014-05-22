2cd52910a278c5765a869c2347d56289f9a0bd35
ullasjos <ujoseph@reisystems.com>
2014-05-22 15:26:33 -0400

    dynamic linking to csv files for the list view

--- roots-nextdatagov/templates/content-opengov.php
+++ roots-nextdatagov/templates/content-opengov.php
@@ -4,8 +4,10 @@
  * User: ujoseph
  * Date: 5/16/14
  * Time: 5:23 PM
- * To change this template use File | Settings | File Templates.
  */
+$international_open_data = (get_option('international_open_data') != '') ? get_option('international_open_data') : '';
+$us_states_open_data = (get_option('us_states_open_data') != '') ? get_option('us_states_open_data') : '';
+$us_counties_open_data = (get_option('us_counties_open_data') != '') ? get_option('us_counties_open_data') : '';
 ?>
 <div class="wrap container">
     <?php
@@ -18,23 +20,23 @@
 <div style="display: none;">
     <div id="data_table_1" class="data_table_opengov">
         <?php
-        displayTable("http://www.data.gov/media/2014/05/us_states.csv",true);
+        displayTable($us_states_open_data,true);
         ?>
     </div>
 </div>
 
 <div style="display: none;">
-    <div id="data_table_2" class="data_table_opengov">
+    <div id="data_table_2"  class="data_table_opengov">
         <?php
-        displayTable("https://www.data.gov/media/2014/05/us_cities_counties.csv",true);
+        displayTable($us_counties_open_data,true);
         ?>
     </div>
 </div>
 
 <div style="display: none;">
-    <div id="data_table_3" class="data_table_opengov">
+    <div id="data_table_3" style="padding: 0 10px;" class="table">
         <?php
-        displayTable("https://www.data.gov/media/2014/05/international.csv",true);
+        displayTable($international_open_data,true);
         ?>
     </div>
 </div>
