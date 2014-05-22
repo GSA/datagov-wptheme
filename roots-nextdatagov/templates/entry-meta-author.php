d154982b42464987ed2bd53f751f638292623bfa
ullasjos <ujoseph@reisystems.com>
2014-05-22 10:09:54 -0400

    added condition if both additional author and author name empty.

--- roots-nextdatagov/templates/entry-meta-author.php
+++ roots-nextdatagov/templates/entry-meta-author.php
@@ -1,7 +1,7 @@
 <?php
 $additional_author_name = get_field('author_name');
 $author_name = get_the_author();
-$display_author_name = (!empty($additional_author_name)) ? "By ".$additional_author_name : "By ".$author_name;
+$display_author_name = (!empty($additional_author_name)) ? "By ".$additional_author_name : (!empty($author_name))? "By ".$author_name : "";
 ?>
 <div class="entry-meta" xmlns="http://www.w3.org/1999/html">
     <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?>&nbsp;&nbsp;<i><?php echo $display_author_name;?></i></time>
