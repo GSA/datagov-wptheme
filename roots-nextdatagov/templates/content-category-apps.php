a8ee8efd731d8a822b29352882bef4d355d46ee8
Philip Ashlock <phil@civicagency.org>
2014-07-14 12:22:13 -0400

    Use subnav include for apps template

--- roots-nextdatagov/templates/content-category-apps.php
+++ roots-nextdatagov/templates/content-category-apps.php
@@ -1,16 +1,15 @@
 <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/jquery.bxslider.js"></script>
 <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/assets/css/jquery.bxslider.css">
+
 <?php
 $category = get_the_category();
-$term_name = $category[0]->cat_name;
-$term_slug = $category[0]->slug;
-?>
-<?php
-$cat_name = $category[0]->cat_name;
 $cat_slug = $category[0]->slug;
-$allowed_slug_arrays = array("climate-ecosystems","coastalflooding","energysupply","foodsupply","humanhealth","transportation","water","climate");
-
 ?>
+
+
+<?php include('category-subnav.php'); ?>
+
+
 <script type="text/javascript">
 
 
@@ -34,28 +33,7 @@ $allowed_slug_arrays = array("climate-ecosystems","coastalflooding","energysuppl
         }
     });
 </script>
-<div class="subnav banner">
-    <div class="container">
-        <nav role="navigation" class="topic-subnav">
-            <ul class="nav navbar-nav">
-                <?php
-                // show Links associated to a community
-                // we need to build $args based either term_name or term_slug
-                if(in_array($term_slug,$allowed_slug_arrays))
-                    wp_nav_menu(array('theme_location' => 'climate_navigation', 'menu_class' => 'nav','items_wrap' => '%3$s'));
-                if(!empty($term_slug)){
-                    $args = array(
-                        'category_name'=> $term_slug, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
-                    wp_list_bookmarks($args);
-                }
-                if (strcasecmp($term_name,$term_slug)!=0) {
-                    $args = array(
-                        'category_name'=> $term_name, 'categorize'=>0, 'title_li'=>0,'orderby'=>'rating');
-                    wp_list_bookmarks($args);
-                }
-                ?>
-            </ul></nav></div>
-</div>
+
 <div class="container">
 <?php
 while( have_posts() ) {
@@ -72,7 +50,7 @@ while( have_posts() ) {
 
 
 
-<!-- Mobile Apps styles --->
+<!-- Mobile Apps styles -->
 <?php
 $args = array(
     'post_type' => 'Applications',
