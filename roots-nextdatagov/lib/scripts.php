37a79f7b4e9429d3cce8aed15b713f4add13450e
Philip Ashlock <phil@civicagency.org>
2014-07-15 12:43:13 -0400

    Fixing content page margin css issue

--- roots-nextdatagov/lib/scripts.php
+++ roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,7 @@ function roots_scripts() {
 	wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700' );
 	//wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
 
-	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd4a6b33e480274db2392791e224506d9');
+	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1427d8a0237d89ec37973b2fe37b4b0d');
 	wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '' );
 
 
