37ad851946b32ee415cd99263f114a48d7682724
kapil-bulchandani <kapil.bulchandani@reisystems.com>
2014-07-17 08:53:30 -0400

    adding survey code

--- roots-nextdatagov/templates/footer.php
+++ roots-nextdatagov/templates/footer.php
@@ -123,4 +123,17 @@ $ckan_default_server = '//' . $ckan_default_server;
         </div>
     </div>
 </footer>
+<div id="survey_target" style="border:none !important;"></div>
+<noscript>
+ <iframe src="http://survey.usa.gov/surveys/161"></iframe>
+</noscript>
+/* hack to fix the border on home page */
+<style>
+ .banner{border:none; paddingbottom:0px;}
+ .frontpagesearch, body.home .header.banner.pageheading {
+ borderbottom: 1px solid #CCCCCC;
+ }
+</style>
+<script type="text/javascript" src="http://survey.usa.gov/widget/161/invitation.js?target_id=survey_target&stylesheet=<?php echo get_template_directory_uri() . '/assets/css/survey.css'; ?>"></script>
+
 <?php wp_footer(); ?>
