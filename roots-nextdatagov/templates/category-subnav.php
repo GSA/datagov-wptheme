b2d741a770b089bfd88c6bc0dcaafc504747bdda
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2014-07-25 19:28:31 -0400

    adding code to append parameter to qa

--- roots-nextdatagov/templates/category-subnav.php
+++ roots-nextdatagov/templates/category-subnav.php
@@ -142,7 +142,11 @@ if ( $subnav OR ( isset( $subnav_extra ) && $subnav_extra ) ):
             e.preventDefault();
             window.location.href = $(this).attr("href")+'#'+ comm;
         });
-
+        $('#main a[href*="//qa-catalog-fe-data.reisys.com/"]').click(function(e) {
+            var comm= 'topic=<?php echo $term_slug; ?>_navigation';
+            e.preventDefault();
+            window.location.href = $(this).attr("href")+'#'+ comm;
+        });
     });
 
 </script>
