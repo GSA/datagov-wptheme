59f4cd358082ff3568805544b7feacdab8bd5ca6
alex-perfilov-reisys <alexandr.perfilov@reisystems.com>
2014-08-26 15:30:30 -0400

    subnav fix

--- roots-nextdatagov/templates/category-subnav.php
+++ roots-nextdatagov/templates/category-subnav.php
@@ -1,23 +1,22 @@
 <?php
 
 if (is_category()) {
-	$cat_ID = get_query_var( 'cat' );
+    $cat_ID = get_query_var('cat');
 
-	$category =  get_category( $cat_ID );
-	$term_name = $category->cat_name;
-	$term_slug = $category->slug;
+    $category  = get_category($cat_ID);
+    $term_name = $category->cat_name;
+    $term_slug = $category->slug;
 
-} else {
-    $category_slug = get_query_var('category_name');
-    if (strpos($category_slug,'/')) {
+} elseif ($category_slug = get_query_var('category_name')) {
+    if (strpos($category_slug, '/')) {
         list(, $sub_category_slug) = explode('/', $category_slug);
         $category_slug = $sub_category_slug;
     }
-    $category = get_category_by_slug($category_slug);
+    $category  = get_category_by_slug($category_slug);
     $term_name = $category->cat_name;
     $term_slug = $category_slug;
-    if(strpos($term_slug, '/') !== FALSE ){
-        $term_slug = substr(strstr($term_slug, '/'),1);
+    if (strpos($term_slug, '/') !== false) {
+        $term_slug = substr(strstr($term_slug, '/'), 1);
     }
 }
 
@@ -25,137 +24,154 @@ if (is_category()) {
 // show Links associated to a community
 // we need to build $args based either term_name or term_slug
 $args = array(
-	'category_name' => $term_slug,
-	'categorize'    => 0,
-	'title_li'      => 0,
-	'echo'          => 0,
-	'orderby'       => 'rating'
+    'category_name' => $term_slug,
+    'categorize'    => 0,
+    'title_li'      => 0,
+    'echo'          => 0,
+    'orderby'       => 'rating'
 );
-if(!empty($term_slug)) {
-    $subnav = wp_list_bookmarks( $args );
+if (!empty($term_slug)) {
+    $subnav = wp_list_bookmarks($args);
 }
 
-if ( strcasecmp( $term_name, $term_slug ) != 0 ) {
-	$args = array(
-		'category_name' => $term_name,
-		'categorize'    => 0,
-		'title_li'      => 0,
-		'echo'          => 0,
-		'orderby'       => 'rating',
-	);
+if (strcasecmp($term_name, $term_slug) != 0) {
+    $args = array(
+        'category_name' => $term_name,
+        'categorize'    => 0,
+        'title_li'      => 0,
+        'echo'          => 0,
+        'orderby'       => 'rating',
+    );
 
-	$subnav_extra = wp_list_bookmarks( $args );
+    $subnav_extra = wp_list_bookmarks($args);
 }
 $allowed_slug_arrays = array(
-	"climate-ecosystems",
-	"coastalflooding",
-	"energysupply",
-	"foodsupply",
-	"humanhealth",
-	"transportation",
-	"water",
-	"climate"
+    "climate-ecosystems",
+    "coastalflooding",
+    "energysupply",
+    "foodsupply",
+    "humanhealth",
+    "transportation",
+    "water",
+    "climate"
 );
-if ( $subnav OR ( isset( $subnav_extra ) && $subnav_extra ) ):
-	?>
+if ($subnav OR (isset($subnav_extra) && $subnav_extra)):
+    ?>
 
-	<div class="subnav banner">
-		<div class="container">
+    <div class="subnav banner">
+        <div class="container">
 
-			<?php if ( $subnav ): ?>
+            <?php if ($subnav): ?>
 
-				<nav class="topic-subnav" role="navigation">
-					<ul class="nav navbar-nav">
-						<?php
-						if ( in_array( $term_slug, $allowed_slug_arrays ) ) {
-							wp_nav_menu( array(
-								'theme_location' => 'climate_navigation',
-								'menu_class'     => 'nav',
-								'items_wrap'     => '%3$s'
-							) );
-						}
-						?>
-						<?php echo $subnav ?>
-					</ul>
-				</nav>
+                <nav class="topic-subnav" role="navigation">
+                    <ul class="nav navbar-nav">
+                        <?php
+                        if (in_array($term_slug, $allowed_slug_arrays)) {
+                            wp_nav_menu(
+                                array(
+                                    'theme_location' => 'climate_navigation',
+                                    'menu_class'     => 'nav',
+                                    'items_wrap'     => '%3$s'
+                                )
+                            );
+                        }
+                        ?>
+                        <?php echo $subnav ?>
+                    </ul>
+                </nav>
 
-			<?php endif; ?>
+            <?php endif; ?>
 
-			<?php if ( isset( $subnav_extra ) && $subnav_extra ): ?>
-				<nav class="topic-subnav" role="navigation">
-					<ul class="nav navbar-nav">
-						<?php
-						if ( in_array( $term_slug, $allowed_slug_arrays ) ) {
-							wp_nav_menu( array(
-								'theme_location' => 'climate_navigation',
-								'menu_class'     => 'nav',
-								'items_wrap'     => '%3$s'
-							) );
-						}
-						?>
-						<?php echo $subnav_extra ?>
-					</ul>
-				</nav>
-			<?php endif; ?>
+            <?php if (isset($subnav_extra) && $subnav_extra): ?>
+                <nav class="topic-subnav" role="navigation">
+                    <ul class="nav navbar-nav">
+                        <?php
+                        if (in_array($term_slug, $allowed_slug_arrays)) {
+                            wp_nav_menu(
+                                array(
+                                    'theme_location' => 'climate_navigation',
+                                    'menu_class'     => 'nav',
+                                    'items_wrap'     => '%3$s'
+                                )
+                            );
+                        }
+                        ?>
+                        <?php echo $subnav_extra ?>
+                    </ul>
+                </nav>
+            <?php endif; ?>
 
-		</div>
-	</div>
+        </div>
+    </div>
 
 <?php else: ?>
 
 
-	<?php
+    <?php
 
-		$valid_sub_menu = false;
+    $valid_sub_menu = false;
 
-		if(!empty($term_slug)) {
+    if (!empty($term_slug)) {
 
-			$sub_menu = wp_nav_menu( array('menu' => $term_slug, 'echo' => false, 'fallback_cb' => '', 'menu_class' => 'nav navbar-nav') );
+        $sub_menu = wp_nav_menu(
+            array('menu' => $term_slug, 'echo' => false, 'fallback_cb' => '', 'menu_class' => 'nav navbar-nav')
+        );
 
-			$expected_html = 'ul id="menu-' . $term_slug;
+        $expected_html = 'ul id="menu-' . $term_slug;
 
-			// if there's no menu, check to see if there's one for the parent category
-			if (!empty($category->category_parent) && (empty($sub_menu) || (!empty($sub_menu) && strpos($sub_menu, $expected_html) != 1))) {			    
-			    $parent_category =  get_category( $category->category_parent );
-			    $expected_html = 'ul id="menu-' . $parent_category->slug;
-				$sub_menu = wp_nav_menu( array('menu' => $parent_category->slug, 'echo' => false, 'fallback_cb' => '', 'menu_class' => 'nav navbar-nav') );				
-			}
+        // if there's no menu, check to see if there's one for the parent category
+        if (!empty($category->category_parent) && (empty($sub_menu) || (!empty($sub_menu) && strpos(
+                        $sub_menu,
+                        $expected_html
+                    ) != 1))
+        ) {
+            $parent_category = get_category($category->category_parent);
+            $expected_html   = 'ul id="menu-' . $parent_category->slug;
+            $sub_menu        = wp_nav_menu(
+                array(
+                    'menu'        => $parent_category->slug,
+                    'echo'        => false,
+                    'fallback_cb' => '',
+                    'menu_class'  => 'nav navbar-nav'
+                )
+            );
+        }
 
-			if (!empty($sub_menu) && strpos($sub_menu, $expected_html) == 1) {
-				$valid_sub_menu = true;
+        if (!empty($sub_menu) && strpos($sub_menu, $expected_html) == 1) {
+            $valid_sub_menu = true;
 
-				if(!empty($parent_category)){
-					$term_slug = $parent_category->slug;
-				}
-			}
+            if (!empty($parent_category)) {
+                $term_slug = $parent_category->slug;
+            }
+        }
 
-		}
+    }
 
-	?>
-
-
-	<?php if($valid_sub_menu): ?>
+    ?>
 
 
-		<div class="subnav banner">
-			<div class="container">
-				<nav class="topic-subnav" role="navigation">
-					<?php echo $sub_menu; ?>
-				</nav>
-			</div>
-		</div>
+    <?php if ($valid_sub_menu): ?>
 
-	<?php endif; ?>
+
+        <div class="subnav banner">
+            <div class="container">
+                <nav class="topic-subnav" role="navigation">
+                    <?php echo $sub_menu; ?>
+                </nav>
+            </div>
+        </div>
+
+    <?php endif; ?>
 
 
 <?php endif; ?>
 <!-- adding parameter to url which passes the term slug for the cross site navigation-->
 <script type="text/javascript">
-    jQuery(function($){
+    jQuery(function ($) {
         var comm = 'topic=<?php echo $term_slug; ?>_navigation';
 
-        $('#main a[href*="catalog.data.gov"]').add('#main a[href*="fe-data.reisys.com"]').not('#main a[href*="#topic"]').each(function(){
-            var newHref = $(this).attr('href') +'#topic=<?php echo $term_slug; ?>_navigation';
+        $('#main a[href*="catalog.data.gov"]').add('#main a[href*="fe-data.reisys.com"]').not('#main a[href*="#topic"]').each(function () {
+            var newHref = $(this).attr('href') + '#topic=<?php echo $term_slug; ?>_navigation';
             $(this).attr('href', newHref);
         });
     });
