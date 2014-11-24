a71d5c840a4bba10613d446ad2b091618c81691d
Philip Ashlock <phil@ashlock.us>
2014-11-24 17:16:08 -0500

    Updates frontpage and nav topic icon display

--- roots-nextdatagov/front-page.php
+++ roots-nextdatagov/front-page.php
@@ -37,14 +37,18 @@ $args = array(
 	<li class="topic-food">
 		<a href="/food"><i></i><span>Agriculture</span></a>
 	</li>
-
+	<li class="topic-business">
+		<a href="/business"><i></i><span>Business</span></a>
+	</li>
 	<li class="topic-climate">
 		<a href="/climate"><i></i><span>Climate</span></a>
 	</li>	
-
-	<!--<li class="topic-consumer">
-		<a href="/consumer"><i></i><span>Consumer</span></a>
-	</li>-->
+    <li class="topic-consumer">
+        <a href="/consumer"><i></i><span>Consumer</span></a>
+    </li>	
+	<li class="topic-ecosystems">
+		<a href="/ecosystems"><i></i><span>Ecosystems</span></a>
+	</li>    
 	<li class="topic-education">
 		<a href="/education"><i></i><span>Education</span></a>
 	</li>
@@ -54,77 +58,25 @@ $args = array(
 	<li class="topic-finance">
 		<a href="/finance"><i></i><span>Finance</span></a>
 	</li>
-	<li class="topic-geospatial">
-		<a href="/geospatial"><i></i><span>Geospatial</span></a>
-	</li>	
-	<li class="topic-development">
-		<a href="/development"><i></i><span>Global Development</span></a>
-	</li>
 	<li class="topic-health">
 		<a href="/health"><i></i><span>Health</span></a>
 	</li>
-	<li class="topic-jobs-and-skills">
-		<a href="/jobs-and-skills"><i></i><span>Jobs &amp; Skills</span></a>
-	</li>
-	
+	<li class="topic-local">
+		<a href="/local"><i></i><span>Local Government</span></a>
+	</li>		
+	<li class="topic-manufacturing">
+		<a href="/manufacturing"><i></i><span>Manufacturing</span></a>
+	</li>	
+	<li class="topic-ocean">
+		<a href="/ocean"><i></i><span>Ocean</span></a>
+	</li>		
 	<li class="topic-safety">
 		<a href="/safety"><i></i><span>Public Safety</span></a>
 	</li>
 	<li class="topic-research">
 		<a href="/research"><i></i><span>Science &amp; Research</span></a>
-	</li>
-	<li class="topic-weather">
-		<a href="/weather"><i></i><span>Weather</span></a>
-	</li>
+	</li>	
 </ul> 
-
-
-<div id="topic-list">
-
-
-<ul class="topics collapse" id="more-topics">
-	<li class="topic-business">
-		<a href="/business"><i></i><span>Business</span></a>
-	</li>
-	<li class="topic-cities">
-		<a href="/cities"><i></i><span>Cities</span></a>
-	</li>
-   <!-- <li class="topic-climate">
-        <a href="/climate"><i></i><span>Climate</span></a>
-    </li>-->
-    <li class="topic-consumer">
-        <a href="/consumer"><i></i><span>Consumer</span></a>
-    </li>
-	<li class="topic-counties">
-		<a href="/counties"><i></i><span>Counties</span></a>
-	</li>
-	<li class="topic-ethics">
-		<a href="https://explore.data.gov/ethics"><i></i><span>Ethics</span></a>
-	</li>
-	<li class="topic-law">
-		<a href="/law"><i></i><span>Law</span></a>
-	</li>	
-	<li class="topic-manufacturing">
-		<a href="/manufacturing"><i></i><span>Manufacturing</span></a>
-	</li>
-	<li class="topic-ocean">
-		<a href="/ocean"><i></i><span>Ocean</span></a>
-	</li>	
-	<li class="topic-states">
-		<a href="/states"><i></i><span>States</span></a>
-	</li>
-</ul>
-
-<div class="more-link">
-    <h5>
-    	<a data-toggle="collapse" data-parent="#topic-list" href="#more-topics" class="collapsed">
-    		<span>More Topics</span>
-    		<i></i>
-    	</a>
-   	</h5>
-</div>
-
-</div>
 
 </div><!--/.container-->
 
