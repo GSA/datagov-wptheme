05ba3ebc03306086e57b98e5dc2b2bbb065b4306
Dan Olaru <dan.olaru@reisystems.com>
2015-05-20 18:33:00 -0400

    revert link url logic

--- roots-nextdatagov/templates/content.php
+++ roots-nextdatagov/templates/content.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
     <header>
         <h2 class="entry-title">
-	    <a id="<?php echo 'post-title-' . get_the_ID(); ?>" href="<?php echo str_replace(home_url(), "", get_permalink()); ?>"><?php the_title(); ?></a>
+	    <a id="<?php echo 'post-title-' . get_the_ID(); ?>" href="<?php echo $post->post_name; ?>"><?php the_title(); ?></a>
 	</h2>
         <?php get_template_part('templates/entry-meta-author'); ?>
     </header>
