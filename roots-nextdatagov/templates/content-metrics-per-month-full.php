8e104583e7f3173adff9c5415834753530023304
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2015-08-25 13:55:34 -0400

    fixing the space

--- roots-nextdatagov/templates/content-metrics-per-month-full.php
+++ roots-nextdatagov/templates/content-metrics-per-month-full.php
@@ -45,9 +45,9 @@ if (!$metrics) {
         $current = date('Y');
         for ($i=$start;$i<= $current; $i++){
             if ($i == $current){
-                echo '<li class="active"><a href="#"><i class="fa fa-minus-circle" style="margin-right:5px">'.$i.'</i></a></li>';
+                echo '<li class="active"><a href="#"><i class="fa fa-minus-circle">&nbsp;'.$i.'</i></a></li>';
             }else{
-                echo '<li><a href="#"><i class="fa fa-plus-circle" style="margin-right:5px">'.$i.'</i></a></li>';
+                echo '<li><a href="#"><i class="fa fa-plus-circle">&nbsp;'.$i.'</i></a></li>';
             }
         }
         ?>
