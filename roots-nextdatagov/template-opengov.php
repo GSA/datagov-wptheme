76d521c8c6a9578ced722e7e9a20826f6390543c
John Yu <john.yu@reisystems.com>
2017-04-27 11:12:56 -0400

    leaflet map deleted from this branch

--- roots-nextdatagov/template-opengov.php
+++ roots-nextdatagov/template-opengov.php
@@ -6,28 +6,3 @@ Template Name: OpenGov
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'opengov'); ?> 
 
-<link rel="stylesheet" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/leaflet/leaflet.css" />
-<link rel="stylesheet" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/leaflet-search/src/leaflet-search.css" />
-<link rel="stylesheet" type="text/css" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.ZoomBox/L.Control.ZoomBox.css">
-<link rel="stylesheet" type="text/css" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.defaultextent/dist/leaflet.defaultextent.css">
-<link rel="stylesheet" type="text/css" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.Pancontrol/src/L.Control.Pan.css">
-<link rel="stylesheet" type="text/css" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.vector-markers/dist/leaflet-vector-markers.css">
-<link rel="stylesheet" type="text/css" href="../app/themes/roots-nextdatagov/assets/LeafletMap2/style.css">
-
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/leaflet/leaflet.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/leaflet-search/src/leaflet-search.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.ZoomBox/L.Control.ZoomBox.min.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.defaultextent/dist/leaflet.defaultextent.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.Pancontrol/src/L.Control.Pan.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/Leaflet.vector-markers/dist/leaflet-vector-markers.min.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/data/international.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/data/us_states.js"></script>
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/data/us_cities_counties.js"></script>
-
-<div class = "container-fluid">
-    <div class = "col-sm-12">	
-        <div id="mapid"></div>
-    </div>
-</div>
-	
-<script src="../app/themes/roots-nextdatagov/assets/LeafletMap2/main.js"></script>
