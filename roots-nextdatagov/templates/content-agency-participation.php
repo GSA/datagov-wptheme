81f214f3d9945ddeb47646e5c560418113aa9326
John Yu <john.yu@reisystems.com>
2017-06-21 12:31:37 -0400

    margin fix

--- roots-nextdatagov/templates/content-agency-participation.php
+++ roots-nextdatagov/templates/content-agency-participation.php
@@ -100,7 +100,7 @@ while (have_posts()) {
     </div>
 </div>
 
-<div class="col-md-5 col-xs-12 data_last_updated" style="margin-top:1%">
+<div class="col-md-5 col-xs-12 data_last_updated" style="margin-top:3%">
 <?php
 $metric_sync = get_option('metrics_updated_gmt');
 echo '<div style="font-style:italic;clear:both;">';
