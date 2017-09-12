016e89edbf0c0cd38620b05e235cc160f6e18d90
John Yu <john.yu@reisystems.com>
2017-09-12 10:40:54 -0400

    metrics subnav added

---
+++ roots-nextdatagov/templates/category-metrics-subnav.php
@@ -0,0 +1,11 @@
+<div class="subnav banner">
+    <div class="container">
+        <nav class="topic-subnav" role="navigation">
+            <ul id="menu-arctic" class="nav navbar-nav">
+                <li class=""><a href="/metrics/">Agency Participation</a></li>
+                <li class=""><a href="/metric/federalagency/dataset-published-per-month/">Datasets Published Per Month</a></li>
+                <li class=""><a href="https://catalog.data.gov/site-usage">Site Usage</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
