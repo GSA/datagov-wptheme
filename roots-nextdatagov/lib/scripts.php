0753eea2aeda8e35e257a5476d0361218da215d1
jbrown-xentity <jbrown@xentity.com>
2020-08-05 12:10:37 -0600

    Updated icon styling

--- roots-nextdatagov/lib/scripts.php
+++ roots-nextdatagov/lib/scripts.php
@@ -14,7 +14,7 @@ function roots_scripts() {
 	wp_enqueue_style('datagov-googlefonts', '//fonts.googleapis.com/css?family=Abel|Lato:100,300,400,700' );
 	//wp_enqueue_style('fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
 
-	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3c5deb08e1805f39213846d8a3a6ff74');
+	wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '46053bb4327f83a4f2f888822950de10');
 	wp_enqueue_style('rei_css', get_template_directory_uri() . '/assets/css/rei.css', false, '' );
 
 
